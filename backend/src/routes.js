c56979652733b78bef033dee16985650ac8fe246
Oseias447 <oseiass.net@gmail.com>
2020-03-27 10:47:48 -0300

    Funcionalidades Avançadas e Testes automatizados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-const OngController = require("../src/controllers/OngCrontrolle");
+const { celebrate, Segments, Joi } = require("celebrate");
+const OngController = require("./controllers/OngCrontroller");
 const IncidentController = require("../src/controllers/IncidentController");
 const ProfileController = require("../src/controllers/ProfileController");
 const SessionController = require("../src/controllers/SessionController");
@@ -10,13 +11,60 @@ const routes = express.Router();
 routes.use(express.json());
 
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", OngController.create);
+
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.string()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
 
 routes.post("/incidents", IncidentController.create);
-routes.get("/incidents", IncidentController.index);
-routes.delete("/incidents/:id", IncidentController.delete);
 
-routes.get("/profile", ProfileController.index);
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.index
+);
+
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentController.delete
+);
+
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
+
 routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
